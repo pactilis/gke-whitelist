59c4ce49634b8749c3875c3238214c35ebd80e5f
Jonatan Sombié <jonatan.sombie@gmail.com>
2020-05-28 15:25:24 +0200

    fist commit. a simple implem

---
+++ README.md
@@ -0,0 +1,37 @@
+# Whitelist/Unwhitelist ip-range on GKE
+
+This action whitelistor un-whitelist ip/range on GKE private cluster
+
+## Inputs
+
+### `zone`
+
+**Required** Google Cloud zone.
+
+### `cluster_id`
+
+**Required** Google Cloud Kubernetes Cluster ID.
+
+### `whitelist`
+
+Whether to whitelist or the other way arround. Default: "true"
+
+### `cidr`
+
+**Required** Ip range to whitelist or unwhitelist
+
+### `cidr`
+
+Display name for the ip range. Default: "Github Action"
+
+## Example usage
+
+You have to make sure `GOOGLE_APPLICATION_CREDENTIALS` is defined, from a previous step for example.
+You may use [setup-gcloud](https://github.com/GoogleCloudPlatform/github-actions/tree/master/setup-gcloud) action and `export_default_credentials` parameter to true.
+
+uses: pactilis/gke-whitelist@master
+with:
+zone: 'my-gcloud-zone'
+cluster_id: 'my-cluster-id'
+whitelist: 'true'
+cidr: '0.0.0.0/32'
---
+++ index.js
@@ -0,0 +1,74 @@
+const container = require("@google-cloud/container");
+const core = require("@actions/core");
+
+const client = new container.v1.ClusterManagerClient();
+const zone = core.getInput("zone");
+const clusterId = core.getInput("cluster_id");
+const cidr = core.getInput("cidr");
+const whitelist = core.getInput("whitelist") === "true";
+const whitelist = core.getInput("name");
+
+if (whitelist) {
+  performWhitelist(cidr, name).catch((err) => {
+    core.setFailed(err);
+  });
+} else {
+  performUnwhitelist(cidr).catch((err) => {
+    core.setFailed(err);
+  });
+}
+
+/**
+ * Whitelist a ip address on GKE private cluster master
+ * @param {String} ipAddress Ip address to whitelist
+ * @param {String} displayName name to give to the entry
+ */
+async function performWhitelist(ipAddress, displayName) {
+  const projectId = await client.getProjectId();
+  const request = {
+    projectId,
+    zone,
+    clusterId,
+  };
+  const [{ masterAuthorizedNetworksConfig }] = await client.getCluster(request);
+  return client.updateCluster({
+    ...request,
+    update: {
+      desiredMasterAuthorizedNetworksConfig: {
+        ...masterAuthorizedNetworksConfig,
+        cidrBlocks: [
+          ...masterAuthorizedNetworksConfig.cidrBlocks,
+          {
+            displayName,
+            cidrBlock: `${ipAddress}/32`,
+          },
+        ],
+      },
+    },
+  });
+}
+
+/**
+ * Remove a cidr block from GKE private cluster master authorized ranges
+ * @param {String} cidrBlock CIDR Block to remove
+ */
+async function performUnwhitelist(cidrBlock) {
+  const projectId = await client.getProjectId();
+  const request = {
+    projectId,
+    zone,
+    clusterId,
+  };
+  const [{ masterAuthorizedNetworksConfig }] = await client.getCluster(request);
+  return client.updateCluster({
+    ...request,
+    update: {
+      desiredMasterAuthorizedNetworksConfig: {
+        ...masterAuthorizedNetworksConfig,
+        cidrBlocks: masterAuthorizedNetworksConfig.cidrBlocks.filter(
+          (entry) => entry.cidrBlock !== cidrBlock
+        ),
+      },
+    },
+  });
+}
